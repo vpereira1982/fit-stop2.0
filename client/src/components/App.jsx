92c0cbd7e87398a02a024a27ea89a136a0ccdef7
Isabella Tea <tea.isabella@gmail.com>
2017-09-23 09:42:43 -0700

    added state rendering functions

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,43 @@
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      isLoggedin: true, //should all start off false, marking dashboard true after this becomes true
+      displayDashboard: true,
+      displayWorkout: false,
+      displaySummary: false
+    };
   }
+
+  handleLoginClick = () => {
+    this.setState({isLoggedin: true});
+  };
+
+  handleLogoutClick = () => {
+    this.setState({isLoggedIn: false});
+  };
+
+  handleWorkoutStartClick = () => {
+    this.setState({displayDashboard: false, displayWorkout: true });
+  };
+
+  handleWorkoutFinish = () => {
+    this.setState({displayWorkout: false, displaySummary: true});
+  };
+
+  handleBackToDashBoardClick = () => {
+    this.setState({displaySummary: false, displayDashboard: true});
+  };
+
+
 
   render() {
     return (
       <div className = "App">
-    	<Header />
-      <Dashboard />
-      <Workout />
-      <Summary />
+    	<Header handleLoginClick={this.handleLoginClick} handleLogoutClick={this.handleLogoutClick} />
+      <Dashboard handleWorkoutStartClick={this.handleWorkoutStartClick} />
+      <Workout handleWorkoutFinish={this.handleWorkoutFinish} />
+      <Summary handleBackToDashBoardClick={this.handleBackToDashBoardClick} />
       </div>
     )
   }
