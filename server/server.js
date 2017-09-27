072a3e0b4fb6e0d7bba7100a9a7bb5fcd9d38e72
harshsikka123 <harshsikka123@gmail.com>
2017-09-26 22:54:35 -0400

    API route for workout history working

--- server/server.js
+++ server/server.js
@@ -15,73 +15,12 @@ app.listen(3000);
 app.use('/public', express.static('client/public'));
 app.use('/react', express.static('node_modules/react/dist'));
 app.use('/react-dom', express.static('node_modules/react-dom/dist'));
-
 app.use('/jquery', express.static('node_modules/jquery/dist'));
 
 console.log('server is running');
 
 
 // define api routes here
-
-//get workout
-
-// function getWorkout(obj, res){ // this disgusting, callback infected function grabs random workout data and sends it to the front for us
-//   Exercise.findOne({type: 'warmup'},function(err,data){
-//     if(err) {
-//       console.log(err)
-//     } else {
-//       obj.warmup.push(data);
-//       Exercise.findOne({type: 'cooldown'},function(err,data){
-//         if(err) {
-//           console.log(err)
-//         } else {
-//           obj.cooldown.push(data);
-//           Exercise.findOne({type: 'workout'},function(err,data){
-//             if(err) {
-//               console.log(err)
-//             } else {
-//               obj.workout.push(data);
-//               Exercise.findOne({type: 'workout'},function(err,data){
-//                 if(err) {
-//                   console.log(err)
-//                 } else {
-//                   obj.workout.push(data);
-//                   Exercise.findOne({type: 'workout'},function(err,data){
-//                     if(err) {
-//                       console.log(err)
-//                     } else {
-//                       obj.workout.push(data);
-//                       obj.counter++
-//                       console.log(obj);
-//                       if(obj.counter === 3) {
-//                         res.send(obj);
-//                       }
-//                     }
-//                   });
-//                 };
-//               })
-//             }
-//           })
-//         }
-//       })
-//     }
-//   })
-
-
-//   {
-//     warmup: [
-//     ],
-//     workout: [
-//     ],
-//     cooldown: [
-//     ]
-//   }
-// 
-// }
-
-
-
-
 
 function getWorkouts(req,res){
   var returnObj = {
@@ -151,5 +90,15 @@ app.get('/workout', getWorkouts);
 
 
 
-app.get('/history',()=>{})
+app.get('/history',(req,res)=>{
+  var name = 'harshsikka' // add req.body.username
+  User.findOne({username: name}, function(err, data){
+    if(err) {
+      console.log('err happened with cooldown retrieval: ' + err);
+    } else{
+      console.log(data.workoutHistory);
+      res.send(data.workoutHistory);
+    }
+  })
+})
 app.post('/addworkout',()=>{})
