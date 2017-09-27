640b5203876c6e8bd888e7914a2afe7487db2c65
harshsikka123 <harshsikka123@gmail.com>
2017-09-26 22:34:36 -0400

    Refactored workout request

--- client/src/components/Summary.jsx
+++ client/src/components/Summary.jsx
@@ -1,7 +1,7 @@
 var Summary = (props) => (
   <div className="summary">
     <h1>Workout Summary</h1>
-    <span className="summaryCongrats">You did it! Congratulations!</span> <br />
+    <span className="summaryCongrats congratulations">You did it! Congratulations!</span> <br />
     <span className="summaryTime">Total Time: 15:00 </span> <br />
     <span className="summaryWorkoutType">Workout Type: Upper Body</span> <br />
     <span className="summaryExercisesCompleted">Exercises Completed: Push Ups, Pull Ups, More Push Ups, Some Other Stuff</span> <br />
