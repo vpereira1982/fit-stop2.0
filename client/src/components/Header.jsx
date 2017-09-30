5a342a5cc14cc1e4424098187c269b9b3e4aad5f
ctnswb <xclai.07@gmail.com>
2017-09-30 11:58:25 -0700

    displays username

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,14 @@
 var Header = (props) => (
   <div className="header">
+
     {props.showButtons && props.loggedIn && (<button className='blackButton' onClick={props.logOut}>Log Out</button>)}
     {props.showButtons && !props.loggedIn && (<button className='blackButton' onClick={props.goToLogin}>Log In</button>)}
-    {props.showButtons && (<button className='blackButton' onClick={props.goToSignUp}>Sign Up</button>)}
+    {!props.loggedIn && props.showButtons && (<button className='blackButton' onClick={props.goToSignUp}>Sign Up</button>)}
 
     <div>
-      <h1> FitStop </h1>
+      <h1> FitStop <span className='username'>{props.username}</span></h1>
+
+
     </div>
   </div>
 );
