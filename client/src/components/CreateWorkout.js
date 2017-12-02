3de6decce513898d158dee3316159ac0ae0b6fb5
Vitor Pereira <vpereira1982@MacBook-Pro.local>
2017-12-02 15:33:25 -0800

    cookieDestroy

--- client/src/components/CreateWorkout.js
+++ client/src/components/CreateWorkout.js
@@ -8,13 +8,22 @@ class CreateWorkout extends React.Component {
       <div>
         <form type="POST" encType="multipart/form-data" action="/createWorkout">
           <div>
-            <label className="createWorkout">Workout Name:
+          <label className="createWorkout">Exercise Type:
+            <select className="workoutType" required>
+              <option value="warmup">Warm Up</option>
+              <option value="workout">Workout</option>
+              <option value="cooldown">Cooldown</option>
+            </select>
+            </label>
+          </div>
+          <div>
+            <label className="createWorkout">Name:
               <input type="text" name="name" required />
             </label>
             <br />
           </div>
             <label className="createWorkout">Description:
-              <input type="text" name="description" required />
+              <input type="text" name="description" className="description" required />
             </label>
             <br />
           <div>
