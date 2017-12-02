5d2f3afd8615d6027eed5e15d3397b778204eec8
lichin shao <lichinshao@gmail.com>
2017-12-01 19:04:50 -0800

    Formatted instructions for each exercise with bullet points

--- client/src/components/Exercise.jsx
+++ client/src/components/Exercise.jsx
@@ -1,12 +1,24 @@
-var Exercise = (props) => (
-  <div className="exercise">
-    <div className="exerciseDescription">
-      <img className="exerciseImage" src={props.exercise.picture} />
-      <p><span className="exerciseName">{props.exercise.name}</span></p>
-      {props.exercise.description}
+var Exercise = (props) => {
+  var description = props.exercise.description;
+  // console.log('description', description)
+  var instructions = description.split('.');
+  // console.log('instructions before', instructions)
+  if (instructions[instructions.length - 1] === '') {
+    instructions = instructions.slice(0, -1);
+  };
+  console.log('instructions', instructions)
+  return(
+    <div className="exercise">
+      <div className="exerciseDescription">
+        <img className="exerciseImage" src={props.exercise.picture} />
+        <p><span className="exerciseName">{props.exercise.name}</span></p>
+        {instructions.map((line, index) => (
+          <li className="exerciseBullet" key={index}>{line}</li>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  )
+};
 
 
 window.Exercise = Exercise;
