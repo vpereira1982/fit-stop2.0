8dfec1988da9f94d9685409a8e0c45e593f88c73
Matthew Anderson <mattander91@gmail.com>
2017-12-05 13:48:31 -0800

    Fixed styling on weight input. History now shows accurate elapsed time.

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -4,8 +4,15 @@ var Dashboard = (props) => (
     <div className="startButton">
       <img onClick={props.goToCountdown} src="public/images/pizzablue.png" alt="Start"/>
     </div>
-    Enter weight (in lbs): <input value={props.userWeight} onChange={props.getUserWeight}/>
-    <History workoutHistory={props.workoutHistory} workoutDate={props.workoutDate} workoutLength={props.workoutLength} loggedIn={props.loggedIn}/>
+    <p></p>
+    <h2>*Enter weight (in lbs):</h2>
+      <p><input value={props.userWeight} onChange={props.getUserWeight}/></p>
+      <p><h5>* To estimate calories burned,</h5></p>
+    <History
+      workoutHistory={props.workoutHistory}
+      workoutDate={props.workoutDate}
+      workoutLength={props.workoutLength}
+      loggedIn={props.loggedIn}/>
   </div>
 );
 
